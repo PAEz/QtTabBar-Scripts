29184dde9f58e69b98760f61061d6cca08cb335c
PAEz <cdkpaez@gmail.com>
2015-07-02 05:25:58 +0930

    google arguments

--- PAEz/Search Google/Google.js
+++ PAEz/Search Google/Google.js
@@ -3,12 +3,21 @@ var shell = WScript.CreateObject("WScript.Shell");
 var fso = new ActiveXObject("Scripting.FileSystemObject");
 var wnd = qs.ActiveWindow;
 
+var args = WScript.Arguments;
+var option = '';
+if (args.Count()){
+	for (var i=0, iEnd=args.Count();i<iEnd;i++){
+		option+='%20'+args.item(i);
+	}
+}
+
 var sel = wnd.selectedItems;
 if (sel.Count) {
 	for (var i = 0; i < sel.Count; i++) {
 		var what = sel.Item(i);
 		var what = fso.GetFileName(sel.item(0));
 		what = encodeURI(what);
-		shell.Run('http://google.com/search?q=' + what, 0);
+		shell.Run('http://google.com/search?q=' + what+option, 0);
+
 	}
 }
--- PAEz/Search Google/README.md
+++ PAEz/Search Google/README.md
@@ -2,3 +2,7 @@ Search Google
 
 Search google for selected file names.
 Each file is a different tab.
+Can take arguments that get added onto the end of the search term.
+So you can create a shortcut with the Arguments as....
+ "Google.js" srt
+...and you can use it to search for subtitles for the file.
